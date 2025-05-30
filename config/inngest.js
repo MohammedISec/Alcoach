1bbc3fdd9ea740e3dd2c61553b5f82fe6f0e7bef
MohammedISec <m7mdm.khalid@gmail.com>
2025-05-30 21:36:19 +0400

    First Commit

---
+++ config/inngest.js
@@ -0,0 +1,67 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "alcoach" });
+
+// Create a client to send events
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "alcoach-client",
+
+    eventHandlerUrl: process.env.NEXT_PUBLIC_INNGEST_EVENT_HANDLER_URL,
+    apiKey: process.env.NEXT_PUBLIC_INNGEST_API_KEY,
+  },
+  {
+    event: "clerk/user.created",
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+    await connectDB();
+    await User.create(userData);
+  }
+);
+// inngest function to handle user updates
+export const syncUserUpdate = inngest.createFunction(
+  {
+    id: "alcoach-client-update",
+  },
+  {
+    event: "clerk/user.updated",
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+    await connectDB();
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+// inngest function to handle user deletion
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: "alcoach-client-delete",
+  },
+  {
+    event: "clerk/user.deleted",
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+    await connectDB();
+    await User.findByIdAndDelete(id);
+  }
+);
